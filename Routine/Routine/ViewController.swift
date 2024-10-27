b685f50ce7b1c51a5ed68ff9b379e27b98eed5f3
이재영 <ldc1000@naver.com>
2024-10-27 17:44:22 +0900

    init

---
+++ Routine/Routine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Routine
+//
+//  Created by t2023-m0072 on 10/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Routine/RoutineTests/RoutineTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineTests.swift
+//  RoutineTests
+//
+//  Created by t2023-m0072 on 10/27/24.
+//
+
+import Testing
+@testable import Routine
+
+struct RoutineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
