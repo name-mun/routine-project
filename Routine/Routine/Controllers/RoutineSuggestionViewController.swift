3c09bd1d855da08b58fb549ceb8725eaffae34a3
name-mun <tains9153@gmail.com>
2024-11-03 16:00:47 +0900

    ✨ RoutineSuggestionViewController 파일 생성

---
+++ Routine/Routine/Controllers/RoutineSuggestionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RoutineSuggestionViewController.swift
+//  Routine
+//
+//  Created by mun on 11/3/24.
+//
+
+import UIKit
+
+class RoutineSuggestionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        }
+}
+
+#Preview("RoutineSuggestionViewController") {
+    RoutineSuggestionViewController()
+}
