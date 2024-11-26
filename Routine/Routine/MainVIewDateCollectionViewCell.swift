a79e7be115e9211c93c9f2850ef93b53d651e6ae
이재영 <ldc1000@naver.com>
2024-11-26 20:37:10 +0900

    [Feat] MainDateCollectionViewCell

---
+++ Routine/Routine/MainVIewDateCollectionViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MainVIewDateCollectionView.swift
+//  Routine
+//
+//  Created by t2023-m0072 on 11/17/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MainVIewDateCollectionViewCell:
+//Data 저장형식 선정
+/*
+ MARK: - Data 저장형식 선정
+ 
+ MARK: - 1.User Defaults
+ 
+ MARK: - 2.Core Data
+ 
+ MARK: - 3.Key Chains
+ 
+ */
+//
+//struct SuggesionData {
+//  var sectionTitle: String
+//  var subTitle: String
+//  
+//  var data: [RoutineData]
+//}
+//
+//let morningRoutine = SuggesionData(sectionTitle: <#T##String#>, subTitle: <#T##String#>, data: <#T##[RoutineData]#>)
