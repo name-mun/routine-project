f2a2ca0676abbc14f4eb54ab9085d0f5c96fd04a
이재영 <ldc1000@naver.com>
2024-12-01 16:00:52 +0900

    Revert "Merge branch 'develop' of https://github.com/name-mun/Routine-Project into feature/RP-26-MainRoutineBoardView"
    This reverts commit 395630218da4f02af7959df1b3b09e831f67cdce, reversing
    changes made to bcb3e88924b611851bcb5ca4272d20a8b2fc1c4d.

---
+++ Routine/Routine/Models/RoutineNotification.swift
@@ -0,0 +1,16 @@
+//
+//  Untitled.swift
+//  Routine
+//
+//  Created by t2023-m0072 on 10/30/24.
+//
+
+import Foundation
+
+//MARK: - 루틴 알림
+//TODO: - 구현 필요
+
+struct RoutineNotification {
+    
+}
+
