0f13197c2c9888be783749cb971c4fbe0a1fb6dc
mun <77603287+name-mun@users.noreply.github.com>
2024-12-01 15:39:53 +0900

    [feature/RoutineData] Configure structure  (#4)
    * ✨ RoutineSuggetionView 파일 생성
    
    * ✨ RoutineSuggestionViewController 파일 생성
    
    * ✨ MainRoutineBoardViewController 파일 생성
    
    - 메인 루틴 보드 뷰 컨트롤러 파일 생성
    
    * [Feat] MainDateCollectionViewCell
    
    * [Feat] RoutineBoardCellPosition 구현
    
    - 셀 포지션 데이터
    
    * [Feat] MainDateCollectionViewCell
    
    * [Feat] RoutineBoardCollectionViewCell
    
    * [Feat] RoutineBoardCellPosition 로직 수정
    
    * wip
    
    * [Fix] 루틴 데이터 형식 1차 수정
    
    * WIP
    
    * 🔧 Configure file  and folder structure
    
    - MVC 패턴으로 폴더를 정리하고 파일을 생성했습니다.
    
    * 🐛 Resolve conflict
    
    * 🔧 Separate TestViewController folder
    
    ---------
    
    Co-authored-by: 이재영 <ldc1000@naver.com>

---
+++ Routine/Routine/TestViewControllers/TestViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Routine
+//
+//  Created by t2023-m0072 on 10/27/24.
+
+import UIKit
+import SnapKit
+
+class TestViewController: UIViewController {
+
+    let goModal = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        goModal.backgroundColor = .black
+        
+        view.addSubview(goModal)
+        
+        goModal.addTarget(self, action: #selector(goNext), for: .touchDown)
+        goModal.snp.makeConstraints() {
+            $0.center.equalToSuperview()
+            
+        }
+        
+    }
+    
+    @objc
+    func goNext() {
+        let modalVC = RoutineSuggestionViewController()
+        
+        modalVC.modalPresentationStyle = .automatic
+        present(modalVC,animated: true,completion: nil)
+        
+    }
+    
+}
+
+
+#Preview("TestViewController") {
+    TestViewController()
+}
