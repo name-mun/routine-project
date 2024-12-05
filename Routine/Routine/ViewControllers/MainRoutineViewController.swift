9ea9ffe646afa968c9c9de9fd4c2a983dbb4bd0f
이재영 <ldc1000@naver.com>
2024-12-05 13:29:50 +0900

    WIP

--- Routine/Routine/ViewControllers/MainRoutineViewController.swift
+++ Routine/Routine/ViewControllers/MainRoutineViewController.swift
@@ -8,16 +8,19 @@
 import UIKit
 import SnapKit
 
+#Preview {
+    MainRoutineViewController()
+}
 
 //MARK: - MainRoutineViewController
 
 // 루틴 메인 화면 ViewController
 class MainRoutineViewController: UIViewController {
     
-    private var routineData: [RoutineData] = []
+    private var routineDatas: [(routine:RoutineData,
+                                result: RoutineResult)] = MockData.routineDatas
     
     private let routineCollectionView: UICollectionView = {
-        
         let collectionView = UICollectionView(frame: .zero,
                                               collectionViewLayout: .init())
         
@@ -51,90 +54,52 @@ class MainRoutineViewController: UIViewController {
         self.view.backgroundColor = .gray
         setRoutineBoardCollectionView()
     }
-  
-  private let collectionViewConstantAbs: CGFloat = 25
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    self.view.backgroundColor = .gray
-    setRoutineBoardCollectionView()
-  }
 }
 
 
 //MARK: - RoutineBoardCollectionViewController
 extension MainRoutineViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-  
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     
-    switch collectionView {
-    case self.routineCollectionView:
-      return 0 //routineData.count
-
-    default:
-      return 0
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        switch collectionView {
+        case self.routineCollectionView:
+            return routineDatas.count
+            
+        default:
+            return 0
+        }
     }
-  }
-  
-  
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     
-    switch collectionView {
-    case self.routineCollectionView:
-      return self.routineBoardCollectionViewCell(indexPath)
-      
-    default:
-      let cell = UICollectionViewCell()
-      cell.backgroundColor = .red
-      return cell
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        switch collectionView {
+        case self.routineCollectionView:
+            return self.routineBoardCollectionViewCell(indexPath)
+            
+        default:
+            let cell = UICollectionViewCell()
+            cell.backgroundColor = .red
+            return cell
+        }
     }
-  }
-  
-  //    func coll
+    
+    //    func coll
 }
 
 
 //MARK: - 루틴보드 컬렉션 뷰 설정
 
 extension MainRoutineViewController {
-  
-  //루틴보드 컬렉션 뷰 설정
-  private func setRoutineBoardCollectionView() {
-    setUpRoutineBoardCollectionView()
-    setRoutineBoardCollectionViewController()
-    setRoutineBoardFlowLayout()
-    registerRoutineBoardCell()
-  }
-  
-  //루틴보드 컬렉션 뷰 UI 설정
-  private func setUpRoutineBoardCollectionView() {
     
-    self.view.addSubview(routineCollectionView)
-    
-    routineCollectionView.snp.makeConstraints { collectionView in
-      collectionView.top.equalTo(300)
-      collectionView.bottom.equalTo(-30)
-      collectionView.leading.trailing.equalToSuperview().inset(collectionViewConstantAbs)
+    //루틴보드 컬렉션 뷰 설정
+    private func setRoutineBoardCollectionView() {
+        setUpRoutineBoardCollectionView()
+        setRoutineBoardCollectionViewController()
+        setRoutineBoardFlowLayout()
+        registerRoutineBoardCell()
     }
-  }
-  
-  //루틴보드 컬렉션 뷰 데이터 소스 & 델리게이트 설정
-  private func setRoutineBoardCollectionViewController() {
-    routineCollectionView.delegate = self
-    routineCollectionView.dataSource = self
-  }
-  
-  //루틴보드 컬렉션 뷰 셀 등록
-  private func registerRoutineBoardCell() {
-    self.routineCollectionView.register(RoutineBoardCollectionViewCell.self,
-                                        forCellWithReuseIdentifier: "RoutineBoardCollectionViewCell")
-  }
-  
-  
-  //루틴보드 컬렉션 뷰 플로우 레이아웃 -> AI 작성 코드 사용 금지 절대 직접 재구현
-    private func setRoutineBoardFlowLayout() {}
-    
     
     //루틴보드 컬렉션 뷰 UI 설정
     private func setUpRoutineBoardCollectionView() {
@@ -144,7 +109,8 @@ extension MainRoutineViewController {
         routineCollectionView.snp.makeConstraints { collectionView in
             collectionView.top.equalTo(300)
             collectionView.bottom.equalTo(-30)
-            collectionView.leading.trailing.equalToSuperview().inset(Self.collectionViewConstantAbs)
+            collectionView.leading.trailing.equalToSuperview()
+                .inset(Self.collectionViewConstantAbs)
         }
     }
     
@@ -156,38 +122,37 @@ extension MainRoutineViewController {
     
     //루틴보드 컬렉션 뷰 셀 등록
     private func registerRoutineBoardCell() {
-        self.routineCollectionView
-            .register(RoutineBoardCollectionViewCell.self,
-                      forCellWithReuseIdentifier: "RoutineBoardCollectionViewCell")
+        self.routineCollectionView.register(RoutineBoardCollectionViewCell.self,
+                                            forCellWithReuseIdentifier: RoutineBoardCollectionViewCell.cellID)
     }
+    
+    
+    //루틴보드 컬렉션 뷰 플로우 레이아웃
+    private func setRoutineBoardFlowLayout() {}
+    
 }
 
 
 //MARK: - 루틴보드 컬렉션 뷰
 
 extension MainRoutineViewController {
-  private func routineBoardCollectionViewCell(_ indexPath: IndexPath) -> RoutineBoardCollectionViewCell {
-    
-    guard let routineBoardCollectionViewCell = self.routineCollectionView.dequeueReusableCell(withReuseIdentifier: "RoutineBoardCollectionViewCell", for: indexPath) as? RoutineBoardCollectionViewCell else {
-      return RoutineBoardCollectionViewCell()
+    private func routineBoardCollectionViewCell(_ indexPath: IndexPath) -> RoutineBoardCollectionViewCell {
+        
+        guard let routineBoardCollectionViewCell = self.routineCollectionView.dequeueReusableCell(withReuseIdentifier: RoutineBoardCollectionViewCell.cellID,
+                                                                                                  for: indexPath) as? RoutineBoardCollectionViewCell else {
+            return RoutineBoardCollectionViewCell()
+        }
+        
+        let index = indexPath.item
+        guard self.routineDatas.count-1 >= index else {
+            return RoutineBoardCollectionViewCell()
+        }
+        
+        let routine = self.routineDatas[index].routine
+        routineBoardCollectionViewCell.configureData(routine)
+        routineBoardCollectionViewCell.configurePosition(indexPath: indexPath,
+                                                         dataCount: routineDatas.count)
+        
+        return routineBoardCollectionViewCell
     }
-    
-    // let index = indexPath.item
-    
-//    guard self.routineData.count-1 >= index else { return RoutineBoardCollectionViewCell() }
-//    
-//    let data = self.routineData[index]
-//    
-//    routineBoardCollectionViewCell.setData(data)
-//    routineBoardCollectionViewCell.setIndex(dataCount: routineData.count, index: index)
-    
-    return routineBoardCollectionViewCell
-  }
-}
-
-
-//MARK: -
-#Preview("MainRoutineViewController") {
-  let viewController = MainRoutineViewController()
-  return viewController
 }
