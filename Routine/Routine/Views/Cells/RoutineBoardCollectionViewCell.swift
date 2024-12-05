24b651181ec91febc4aa57e02a5a438a83cc30af
이재영 <ldc1000@naver.com>
2024-12-05 13:26:47 +0900

    ♻️ RoutineBoardCollectionViewCell
    - 불필요한 코드 제거
    - setUpUI 하나의 메서드로 병합

--- Routine/Routine/Views/Cells/RoutineBoardCollectionViewCell.swift
+++ Routine/Routine/Views/Cells/RoutineBoardCollectionViewCell.swift
@@ -12,22 +12,24 @@ import SnapKit
 
 class RoutineBoardCollectionViewCell: UICollectionViewCell {
     
-    override var reuseIdentifier: String? { "RoutineBoardCollectionViewCell" }
+    static let cellID: String = "RoutineBoardCollectionViewCell"
     
     static let borderWidth: CGFloat = 1
     static let cornerRadius: CGFloat = 20
     
+    //루틴 데이터
     private var routine: RoutineData?
     
+    //전체 스택 뷰
     private var stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.backgroundColor = .clear
         stackView.alignment = .center
         stackView.axis = .vertical
-        
         return stackView
     }()
     
+    //루틴 제목 라벨 뷰
     private let titleLabel: UILabel = {
         let titleLabel = UILabel()
         titleLabel.backgroundColor = .clear
@@ -38,35 +40,35 @@ class RoutineBoardCollectionViewCell: UICollectionViewCell {
         return titleLabel
     }()
     
-    private let stopMarkView: UIImageView = {
+    //스티커 이미지 뷰
+    private let stickerView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .bottom
+        imageView.backgroundColor = .clear
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    //중단 마크 이미지 뷰
+    private let stopMarkImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.backgroundColor = .clear
         imageView.image = UIImage(systemName: "minus.circle")
         imageView.contentMode = .scaleAspectFit
-        imageView.tintColor = .red
         imageView.isHidden = true
         return imageView
     }()
     
-    private let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .bottom
-        imageView.backgroundColor = .clear
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
-    
-    private var isViewSetUp: Bool = false
-    
-    //
-    func setData(_ routine: RoutineData) {
+    ///
+    func configureData(_ routine: RoutineData) {
         self.routine = routine
-        
-        if !isViewSetUp {
-            setUpUI()
-        }
-        
         updateData()
+    }
+    
+    func configurePosition(indexPath: IndexPath, dataCount: Int) {
+        let position = Position(dataCount: dataCount, index: indexPath.item)
+        self.layer.maskedCorners = position.maskedCorner()
+        return
     }
     
     //
@@ -79,100 +81,66 @@ class RoutineBoardCollectionViewCell: UICollectionViewCell {
         resetData()
         updateData()
     }
-}
-
-
-
-//MARK: - round
-extension RoutineBoardCollectionViewCell {
     
-    //
-    private func cornerRound() {
-        self.clipsToBounds = true
-        self.layer.cornerRadius = Self.cornerRadius
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpUI()
     }
     
-    private func setBorder() {
-        self.layer.borderWidth = Self.borderWidth
-        self.layer.borderColor = CGColor.init(red: 0, green: 0, blue: 0, alpha: 1)
-    }
-    
-    //TODO: FlowLayOut 구현 이후 리팩토링 필수(FlowLayOut 구현 이후 사용) (+마지막 셀 라운드 케이스 미구현)
-    func maskedCorner(indexPath: IndexPath, dataCount: Int) {
-        let position = Position(dataCount: dataCount, index: indexPath.item)
-        self.layer.maskedCorners = position.maskedCorner()
-        return
-        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
+
+
 //MARK: - Set Up UI
-//FIXME: View를 확인하고, 반영하는 형태로 수정 필요
 extension RoutineBoardCollectionViewCell {
     
     private func setUpUI() {
-        guard !isViewSetUp else { return }
         
-        setUpStackView()
-        setUpTitleLabel()
-        setUpStickerImageView()
-        setUpStopMarkImageView()
+        [stackView, stopMarkImageView].forEach {
+            addSubview($0)
+        }
         
-        cornerRound()
-        setBorder()
-        
-        isViewSetUp = true
-    }
-    
-    //스택 뷰 UI 설정
-    private func setUpStackView() {
-        
-        self.addSubview(stackView)
+        [titleLabel, stickerView].forEach {
+            stackView.addArrangedSubview($0)
+        }
         
         stackView.snp.makeConstraints { stackView in
             stackView.top.bottom.leading.trailing.equalToSuperview()
         }
-    }
-    
-    //타이틀 라벨 뷰 UI 설정
-    private func setUpTitleLabel() {
-        
-        stackView.addArrangedSubview(titleLabel)
         
         titleLabel.snp.makeConstraints { titleLabel in
             titleLabel.leading.trailing.equalToSuperview().inset(20)
             titleLabel.height.equalToSuperview().multipliedBy(0.7)
             titleLabel.centerX.equalToSuperview()
         }
-    }
-    
-    //스티커 이미지 뷰 UI 설정
-    private func setUpStickerImageView() {
-        stackView.addArrangedSubview(imageView)
         
-        imageView.snp.makeConstraints { imageView in
+        stickerView.snp.makeConstraints { imageView in
             imageView.height.equalToSuperview().multipliedBy(0.3)
             imageView.centerX.equalToSuperview()
         }
-    }
-    
-    private func setUpStopMarkImageView() {
-        self.addSubview(stopMarkView)
         
-        stopMarkView.snp.makeConstraints { imageView in
+        stopMarkImageView.snp.makeConstraints { imageView in
             imageView.trailing.top.equalToSuperview().inset(8)
             imageView.size.height.width.equalTo(15)
         }
-    }
     
+        self.clipsToBounds = true
+        self.layer.cornerRadius = Self.cornerRadius
+        
+        self.layer.borderWidth = Self.borderWidth
+        self.layer.borderColor = CGColor.init(red: 0, green: 0, blue: 0, alpha: 1)
+    }
 }
 
 //MARK: - prepare for reuse
 
 extension RoutineBoardCollectionViewCell {
     private func resetData() {
-        self.imageView.image = nil
+        self.stickerView.image = nil
         self.titleLabel.text = nil
-        self.stopMarkView.isHidden = true
+        self.stopMarkImageView.isHidden = true
     }
 }
 
@@ -187,11 +155,11 @@ extension RoutineBoardCollectionViewCell {
         updateBackgroundColor(routine.color)
         updateTitleLabel(routine.title)
         updateStickerImageView(routine.sticker)
-        updateStopMarkView(routine.stopDate != nil)
+        updateStopMarkView(routine.stopDate == nil)
     }
     
     private func updateBackgroundColor(_ color: BoardColor) {
-        self.backgroundColor = .orange
+        self.backgroundColor = color.get()
     }
     
     private func updateTitleLabel(_ title: String) {
@@ -199,21 +167,17 @@ extension RoutineBoardCollectionViewCell {
     }
     
     private func updateStickerImageView(_ assetName: AssetName) {
-        guard let stickerImage = UIImage(named: assetName) else {
+        guard let stickerImage = UIImage(systemName: assetName)?.withRenderingMode(.alwaysOriginal) else {
             return
         }
-        
-        self.imageView.image = stickerImage
+        self.stickerView.image = stickerImage
     }
     
     private func updateStopMarkView(_ stop: Bool) {
-        stopMarkView.isHidden = false
+        stopMarkImageView.isHidden = stop
     }
     
 }
-
-
-
 
 
 //MARK: - Preview
