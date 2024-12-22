c2ed3b8a63af0a343293ec906201897995c67226
name-mun <tains9153@gmail.com>
2024-12-22 15:10:14 +0900

    ✨ CreateRoutineViewController 구현

--- Routine/Routine/ViewControllers/CreateRoutineViewController.swift
+++ Routine/Routine/ViewControllers/CreateRoutineViewController.swift
@@ -12,21 +12,45 @@ class CreateRoutineViewController: UIViewController {
 
     let routineEditorView = RoutineEditorView()
 
+    override func loadView() {
+        super.loadView()
+
+        view = routineEditorView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureNav()
+        setupAction()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureNav() {
+        self.navigationController?.navigationBar.isHidden = true
     }
-    */
 
+    private func setupAction() {
+        routineEditorView.colorButton.addAction(UIAction{ [weak self] _ in
+            self?.colorButtonTapped()
+        }, for: .touchUpInside)
+    }
+}
+
+extension CreateRoutineViewController {
+    private func colorButtonTapped() {
+        let modalVC = SelectColorViewController()
+        // 모달 화면 크기 설정
+        if let sheet = modalVC.sheetPresentationController {
+            if #available(iOS 16.0, *) {
+                sheet.detents = [
+                    .custom { _ in
+                        return 250
+                    }
+                ]
+            } else {
+                sheet.detents = [.medium()]
+            }
+        }
+        present(modalVC, animated: true)
+    }
 }
